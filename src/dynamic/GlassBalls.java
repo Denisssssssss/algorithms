217d7ed29ab74c5a1e14bb98e6b61090adca9ac4
Denisssssssss <densh2001@mail.ru>
2022-03-18 15:06:17 +0300

    update task 3

--- src/dynamic/GlassBalls.java
+++ src/dynamic/GlassBalls.java
@@ -19,23 +19,29 @@ public class GlassBalls {
         int firstFloor = 1;
         int lastFloor = n;
 
+        System.out.println("Начинаю бросать\n");
         // двоичным поиском находим промежуток, где предположительно разбиваются шары
         while (k > 2) {
             // проверяем, разбился или нет
+            System.out.println("Бросаю с " + (firstFloor + (lastFloor - firstFloor) / 2) + " этажа");
             if (lastFloor / 2 >= criticalHeight) {
+                System.out.println("Разбился");
                 lastFloor = lastFloor / 2 - 1;
             } else {
+                System.out.println("Не разбился");
                 if ((lastFloor - firstFloor) / 2 + firstFloor < criticalHeight) {
                     firstFloor += (lastFloor - firstFloor) / 2;
                 } else {
                     lastFloor -= (lastFloor - firstFloor) / 2;
                 }
             }
+            System.out.println("Теперь бросаю в промежутке [" + firstFloor + ", " + lastFloor + "]\n");
             throwsAmount++;
             k--;
         }
 
         // этот промежуток разделим на несколько отрезков, с концов которых бросаем шары
+        System.out.println("Шар разбивается в этом промежутке: [" + firstFloor + ", " + lastFloor + "]\n");
         int floors = lastFloor - firstFloor + 1;
         BigDecimal eight = new BigDecimal(8);
         BigDecimal d = BigDecimal.ONE.add(eight.multiply(BigDecimal.valueOf(floors)));
@@ -46,26 +52,35 @@ public class GlassBalls {
             section++;
         }
         int minimum = section;
-        System.out.println("Minimum throws: " + (throwsAmount + minimum));
+        System.out.println("Минимум бросков: " + (throwsAmount + minimum) + "\n");
+
+        System.out.println("Теперь бросаю в промежутке " +
+                "[" + (firstFloor + section - 1) + ", " + (firstFloor + 2 * section - 1) + "]\n");
 
         for (int i = firstFloor + section - 1; i <= lastFloor; i += section) {
+            System.out.println("Бросаю с " + i + " этажа");
             throwsAmount++;
             if (i >= criticalHeight) {
+                System.out.println("Разбился");
                 lastFloor = i;
                 break;
             }
+            System.out.println("Не разбился");
             section--;
             firstFloor = i + 1;
+            System.out.println("Теперь бросаю в промежутке " + "[" + firstFloor + ", " + (firstFloor + section - 1) + "]\n");
         }
 
-        System.out.print("Answer is correct: ");
+        System.out.println("Шар разбивается в этом промежутке: [" + firstFloor + ", " + lastFloor + "]\n");
+
         for (int i = firstFloor; i <= lastFloor; i++) {
+            System.out.println("Бросаю с " + i + "этажа");
             if (i == criticalHeight && minimum != 0) {
-                System.out.println(true);
+                System.out.println("\nОтвет: " + i + "этаж");
                 return;
             }
             if (minimum == 0) {
-                System.out.println(false);
+                System.out.println("Исчерпан лимит бросков");
                 return;
             }
             minimum--;
